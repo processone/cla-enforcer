344d8cf87fce972313923d60021b8caf3b2cc3b7
Sébastien Luquet <sluquet@process-one.net>
2025-05-19 18:17:22 +0200

    fix/improve smtp config

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,8 +3,9 @@ ActionMailer::Base.view_paths = File.expand_path('../../views/', __FILE__)
 ActionMailer::Base.smtp_settings = {
   address: ENV['SMTP_ADDRESS'],
   user_name: ENV['SMTP_USERNAME'],
-  password: ENV['SMTP_USERNAME'],
-  port: ENV['SMTP_PORT']
+  password: ENV['SMTP_PASSWORD'],
+  port: ENV['SMTP_PORT'],
+  ssl: true
 }
 
 class ApplicationMailer < ActionMailer::Base
